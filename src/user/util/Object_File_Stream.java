1113eef6e7de765c0c590bb0dc138c66094a74b1
Artxe2 <balgoon24@gmail.com>
2020-07-02 16:24:42 +0900

    2020 0702 1624

---
+++ src/user/util/Object_File_Stream.java
@@ -0,0 +1,68 @@
+package user.util;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+
+public class Object_File_Stream implements Serializable {
+
+	class Data implements Serializable {
+		private String name = "object";
+
+		private void mtd() {
+			System.out.println("im " + name);
+		}
+	}
+
+	private static final File f = new File("D:\\a");
+	private static String s;
+
+	Object_File_Stream() throws Exception {
+		oosFromFile();
+		oisFromFile();
+		oosFromString();
+		oisFromString();
+	}
+
+	public static void main(String[] args) throws Exception {
+		new Object_File_Stream();
+	}
+
+	public void oosFromFile() throws Exception {
+		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(f));
+		Data o = new Data();
+		oos.writeObject(o);
+		oos.close();
+	}
+
+	public void oisFromFile() throws Exception {
+		ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(new FileInputStream(f)));
+		Data o = (Data) ois.readObject();
+		o.mtd();
+		ois.close();
+	}
+
+	public void oosFromString() throws Exception {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		ObjectOutputStream oos = new ObjectOutputStream(new BufferedOutputStream(baos));
+		Data o = new Data();
+		oos.writeObject(o);
+		oos.close();
+		s = new String(baos.toByteArray(), "ISO-8859-1");
+	}
+
+	public void oisFromString() throws Exception {
+		ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(new ByteArrayInputStream(s.getBytes("ISO-8859-1"))));
+		Data o = (Data) ois.readObject();
+		o.mtd();
+		ois.close();
+	}
+}
