705bc274ef78a4515d4ebccdf4110dd475873230
Artxe2 <balgo@DESKTOP-EU0HILL>
2020-07-26 01:36:36 +0900

    asd

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,10 @@
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
-public class Main extends JFrame {
+public class Main {
 
 	public Main() {
-		String s = new java.io.File("img/img.png").getAbsolutePath();
-		setTitle(s);
-		setSize(600, 600);
-		setIconImage(new ImageIcon(s).getImage());
-		add(new JPanel() {
-			@Override
-			protected void paintComponent(java.awt.Graphics g) {
-				g.drawImage(new ImageIcon(s).getImage(), 0, 0, 600, 600, null);
-				setOpaque(false);
-			}
-		});
-		setVisible(true);
+		int num = 9;
+		for (int i = 1; i <= 9; i++) {
+			System.out.println(num + " x " + i + " = " + (i * num));
+		}
 	}
 
 	public static void main(String[] args) {
