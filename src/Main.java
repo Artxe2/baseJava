5d888a20cc3a76e4b40cb3a085ff1837bab85ea4
Artxe2 <balgo@DESKTOP-EU0HILL>
2020-07-25 14:47:04 +0900

    asd

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,25 @@
-interface If {
-	int asd(int a);
-}
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class Main {
+public class Main extends JFrame {
 
-	Main() {
-
+	public Main() {
+		String s = new java.io.File("img/img.png").getAbsolutePath();
+		setTitle(s);
+		setSize(600, 600);
+		setIconImage(new ImageIcon(s).getImage());
+		add(new JPanel() {
+			@Override
+			protected void paintComponent(java.awt.Graphics g) {
+				g.drawImage(new ImageIcon(s).getImage(), 0, 0, 600, 600, null);
+				setOpaque(false);
+			}
+		});
+		setVisible(true);
 	}
 
 	public static void main(String[] args) {
-		If i = (a) -> { return a * 4; };
-		int r = i.asd(4);
-		System.out.println(r);
-		new Thread(() ->  {System.out.println("run");}).start();
+		new Main();
 	}
 }
