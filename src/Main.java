96e890cf40b35f45b049ad8f1bf2759519d90a8f
artxe <balgoon24@gmail.com>
2021-01-10 01:21:17 +0900

    asd

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,19 @@
 class Main {
     public static void main(String[] args) {
+        int hp = 2000;
+        int lostHp = 0;
+        int damage = 200;
+        int result = 0;
+        int start = 0, mid, end = (int) Math.ceil(hp * 0.77);
+        while (start < end) {
+            mid = (start + end + 1) / 2;
+            lostHp = mid;
+            result = (int) ((2 * (lostHp * 100.0 / hp > 77 ? 77 : lostHp * 100.0 / hp) / 77 + 1) * damage);
+            if (result * 4 + lostHp > hp) {
+                end = mid - 1;
+            } else {
+                start = mid;
+            }
+        }
     }
 }
